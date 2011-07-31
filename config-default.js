57527525fb7478b6944d1e5c7628234e2ea65b30
Kurt Raschke <kurt@kurtraschke.com>
2011-07-31 16:24:25 -0400

    Initial commit.

---
+++ config-default.js
@@ -0,0 +1,6 @@
+//Get an API key from http://developer.wmata.com/
+var apikey = "your key here";
+//Get station RTU code from list at http://goo.gl/RRz1T
+//For stations with multiple RTUs, only one code is needed.
+//The other RTU code will be detected automatically.
+var rtu = "station RTU code here";
